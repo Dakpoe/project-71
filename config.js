11816a8e368b5062d147606e21d707cfa317632a
Dakpoe <123876662+Dakpoe@users.noreply.github.com>
2023-12-31 17:29:45 +0000

    Update config.js
    Added Firebase

--- config.js
+++ config.js
@@ -1,9 +1,17 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-//Paste your firebaseConfig here
 
-//
+const firebaseConfig = {
+  apiKey: "AIzaSyAAIHqhQMCHLAReMMhKh3hWhBMmGKV7F3o",
+  authDomain: "bike-online-website-app.firebaseapp.com",
+  projectId: "bike-online-website-app",
+  storageBucket: "bike-online-website-app.appspot.com",
+  messagingSenderId: "568420241873",
+  appId: "1:568420241873:web:b800869d1551bb930210f0",
+  measurementId: "G-23KZH6SY6B"
+};
+
 firebase.initializeApp(firebaseConfig);
 
 export default firebase.firestore();
